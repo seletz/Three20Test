955501a860e4ba2ac6a321e14f167063ca86aa00
Stefan Eletzhofer <se@nexiles.de>
2011-02-23 20:05:09 +0100

    XCode template

---
+++ Classes/Three20TestAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  Three20TestAppDelegate.h
+//  Three20Test
+//
+//  Created by Stephan Eletzhofer on 23.02.11.
+//  Copyright 2011 Nexiles GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Three20TestAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
